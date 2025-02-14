aff0063c95e4d7e2c49e8838c043243d6fb6b28d
Shashank Reddy Nallu <nallushashankreddy@gmail.com>
2025-02-15 04:07:32 +0530

    Env variable changes

--- portfolio/src/environments/environment.ts
+++ portfolio/src/environments/environment.ts
@@ -3,14 +3,14 @@ export const environment = {
     apiUri: "",
     s3AssetUrl: "",
     emailServiceData: {
-        templateId: process.env["EMAIL_TEMPLATE_ID"],
-        apiUrl: process.env["EMAIL_API_URL"],
-        apiKey: process.env["EMAIL_API_KEY"],
+        templateId: parseInt(process.env["EMAIL_TEMPLATE_ID"] || "2"),
+        apiUrl: process.env["EMAIL_API_URL"] || "",
+        apiKey: process.env["EMAIL_API_KEY"] || "",
     },
     socialMediaUrls: {
-        linkedIn: process.env["LINKEDIN"],
-        gitHub: process.env["GITHUB"],
-        instagram: process.env["INSTAGRAM"],
-        youtube: process.env["YOUTUBE"]
+        linkedIn: process.env["LINKEDIN"] || "",
+        gitHub: process.env["GITHUB"] || "",
+        instagram: process.env["INSTAGRAM"] || "",
+        youtube: process.env["YOUTUBE"] || ""
     }
 };
