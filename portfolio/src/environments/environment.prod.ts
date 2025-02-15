7d62d6ead694fb6d16c5328159db1854f8cdd5f4
Shashank Reddy Nallu <nallushashankreddy@gmail.com>
2025-02-15 13:30:37 +0530

    Reset environment files

--- portfolio/src/environments/environment.prod.ts
+++ portfolio/src/environments/environment.prod.ts
@@ -3,14 +3,14 @@ export const environment = {
     apiUri: "",
     s3AssetUrl: "",
     emailServiceData: {
-        templateId: 2,
-        apiUrl: "https://api.brevo.com/v3/smtp/email",
-        apiKey: "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",
+        templateId: 1,
+        apiUrl: 'DEFAULT_API_URL',
+        apiKey: 'DEFAULT_API_KEY',
     },
     socialMediaUrls: {
-        linkedIn: 'https://www.linkedin.com/in/nallu-shashank-reddy',
-        gitHub: 'https://github.com/Shashank-Reddy-Nallu',
-        instagram: 'https://www.instagram.com/shashi._.ig',
-        youtube: 'https://www.youtube.com'
+        linkedIn: 'DEFAULT_URL',
+        gitHub: 'DEFAULT_URL',
+        instagram: 'DEFAULT_URL',
+        youtube: 'DEFAULT_URL'
     }
 };
